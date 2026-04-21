15c7e77f691f8d6cd29a44b9534a5c4f966d7216
Christophe Crokaert <croc1014@me.com>
2026-04-21 17:28:37 +0200

    update import file + method controller

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -20,10 +20,20 @@ class TextsController < ApplicationController
     @text = Text.new(text_params)
     @text.user = current_user
 
+    if params[:text][:source] == "copy"
+      unless @text.content.present?
+        render :new_copy, status: :unprocessable_entity and return
+      end
+    end
+
     if @text.save
       redirect_to text_path(@text), notice: "Texte créé avec succès"
     else
+      if params[:text][:source] == "import"
+        render :new_import, status: :unprocessable_entity
+      else
       render :new, status: :unprocessable_entity
+      end
     end
   end
 
@@ -42,7 +52,7 @@ class TextsController < ApplicationController
   private
 
   def text_params
-    params.require(:text).permit(:title, :content, :favourite, :translated_content)
+    params.require(:text).permit(:title, :content, :favourite, :translated_content, :document)
   end
 
   def set_text
--- app/models/text.rb
+++ app/models/text.rb
@@ -1,4 +1,5 @@
 class Text < ApplicationRecord
   belongs_to :user
-  validates :content, presence: true
+  validates :title, presence: true
+  has_one_attached :document
 end
