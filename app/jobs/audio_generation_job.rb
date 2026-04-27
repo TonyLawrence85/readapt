20ecc6fce7e10ed42d50c7b2fa9554d858f36b5e
Christophe Crokaert <croc1014@me.com>
2026-04-27 18:56:00 +0200

    phase whisper test avant modif tts

--- app/jobs/audio_generation_job.rb
+++ app/jobs/audio_generation_job.rb
@@ -5,21 +5,28 @@ class AudioGenerationJob < ApplicationJob
   queue_as :default
 
   def perform(article_id)
-    article = Article.Find(article_id)
+    article = Article.find(article_id)
 
     return if article.formatted_content.blank?
+    TtsService.call(article.formatted_content)
 
+    article.audio.attach(
+      io: File.open(Rails.root.join("public", "output.mp3")),
+      filename: "article_#{article.id}.mp3",
+      content_type: "audio/mpeg"
+    )
     return unless article.audio.attached?
 
-    client = OpenAI::Client.new(access_token: ENV["OPEN_API_KEY"])
-    audio_url = Rails.application.routes.url_helpers.url_for(articles.audio)
+    client = OpenAI::Client.new(access_token: ENV["OPENAI_API_KEY"])
+    audio_url = Rails.application.routes.url_helpers.url_for(article.audio)
 
+    audio_file = URI.open(audio_url)
 
     response = client.audio.transcribe(
       parameters: {
-        model: "whisper-1"
-        file: URI.open(audio_url),
-        response_format: "verbose_json"
+        model: "whisper-1",
+        file: audio_file,
+        response_format: "verbose_json",
         language: "fr"
       }
     )
@@ -34,3 +41,4 @@ class AudioGenerationJob < ApplicationJob
     article.update(audio_timestamps: timestamps.to_json)
   end
 end
+
