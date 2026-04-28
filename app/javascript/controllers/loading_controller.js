36f372d5b29c43bb67e28e3c272d5d0bdcddc4d9
Cedric GOVAERTS <govaertscedric@gmail.com>
2026-04-28 19:30:53 +0200

    basic loading page

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -1,21 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
-import Typed from 'typed.js';
 // Connects to data-controller="loading"
 export default class extends Controller {
   connect() {
     console.log("hello");
   }
-
   loadingPage() {
     console.log("loading page")
 
-    typed3 = new Typed('#typed3', {
-      strings: ['My strings are: <i>strings</i> with', 'My strings are: <strong>HTML</strong>', 'My strings are: Chars &times; &copy;'],
-      typeSpeed: 0,
-      backSpeed: 0,
-      smartBackspace: true, // this is a default
-      loop: true
-    });
+    typed6 = new Typed('#typed6', {
+    strings: ['Je rephrase le texte en phrase plus courte...\n `Chargement des settings..` ^1000 \n Changement de police d\'écriture`\n`Ajout des couleurs...` ^1000 \n `Ajout des espacements`\n `Modification de la taille`\n `Création du texte en audio...`'],
+    typeSpeed: 100,
+    backSpeed: 0,
+    loop: true
+  });
   }
 }
-  
