abf5bbaca5d5c913c3f4a34ace1c149bb24ba55a
Tony Mouhete <tony.mouhete@gmail.com>
2026-04-28 13:56:15 +0200

    homepage + product checklist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  helper_method :current_setting
 
   def current_setting
     return nil unless user_signed_in?
+
     current_user.setting
   end
-  helper_method :current_setting
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
