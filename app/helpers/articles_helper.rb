bc8ed01911c091d2257e9bc4b0ef284eb98ee5e5
Tony Mouhete <tony.mouhete@gmail.com>
2026-08-22 23:38:40 +0200

    Fix RuboCop style offenses in articles helper

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,8 +1,10 @@
 module ArticlesHelper
   def inject_karaoke_targets(html)
-    sentences = html.split(/<br\s*\/?>/i).reject(&:blank?)
+    sentences = html.split(%r{<br\s*/?>}i).reject(&:blank?)
     sentences.map.with_index do |sentence, i|
-      content_tag(:span, sentence.html_safe,
+      content_tag(
+        :span,
+        sentence.html_safe,
         "data-karaoke-target" => "segment",
         "data-index" => i,
         class: "karaoke-segment"
