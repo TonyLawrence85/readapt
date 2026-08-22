a34a56c15e44fbf85ebc3fa12d187f872c9e13ad
Tony Mouhete <tony.mouhete@gmail.com>
2026-08-22 23:39:26 +0200

    Clean application helper style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-
   def with_px(value)
     v = value.to_s.strip
     return "0px" if v.blank?
+
     v.end_with?("px") ? v : "#{v}px"
   end
 
@@ -14,21 +14,22 @@ module ApplicationHelper
     return html_content unless setting
 
     letter_spacing = with_px(setting.letter_spacing)
-    font_size      = with_px(setting.font_size)
-    palette_data   = TextFormatter.palette_data_for(setting.syllable_palette)
-    colors         = palette_data[:syllables]
+    font_size = with_px(setting.font_size)
+    palette_data = TextFormatter.palette_data_for(setting.syllable_palette)
+    colors = palette_data[:syllables]
 
     base_style = "font-family: #{setting.font}; letter-spacing: #{letter_spacing}; font-size: #{font_size};"
 
     if palette_data[:bg]
-      base_style += " background-color: #{palette_data[:bg]}; color: #{palette_data[:text]}; padding: 1rem; border-radius: 8px;"
+      base_style += " background-color: #{palette_data[:bg]}; color: #{palette_data[:text]}; " \
+                    "padding: 1rem; border-radius: 8px;"
     end
 
     syl_css = if setting.syllable_mode
-      ".syl-0{color:#{colors[0]}}.syl-1{color:#{colors[1]}}.syl-2{color:#{colors[2]}}"
-    else
-      ".syl-0,.syl-1,.syl-2{color:inherit}"
-    end
+                ".syl-0{color:#{colors[0]}}.syl-1{color:#{colors[1]}}.syl-2{color:#{colors[2]}}"
+              else
+                ".syl-0,.syl-1,.syl-2{color:inherit}"
+              end
 
     "<style>#{syl_css}</style><div style='#{base_style}'>#{html_content}</div>".html_safe
   end
@@ -41,21 +42,22 @@ module ApplicationHelper
     return text unless setting
 
     letter_spacing = with_px(setting.letter_spacing)
-    font_size      = with_px(setting.font_size)
-    palette_data   = TextFormatter.palette_data_for(setting.syllable_palette)
-    colors         = palette_data[:syllables]
+    font_size = with_px(setting.font_size)
+    palette_data = TextFormatter.palette_data_for(setting.syllable_palette)
+    colors = palette_data[:syllables]
 
     base_style = "font-family: #{setting.font}; letter-spacing: #{letter_spacing}; font-size: #{font_size};"
 
     if palette_data[:bg]
-      base_style += " background-color: #{palette_data[:bg]}; color: #{palette_data[:text]}; padding: 0.8rem 1rem; border-radius: 8px;"
+      base_style += " background-color: #{palette_data[:bg]}; color: #{palette_data[:text]}; " \
+                    "padding: 0.8rem 1rem; border-radius: 8px;"
     end
 
     syl_css = if setting.syllable_mode
-      ".syl-0{color:#{colors[0]}}.syl-1{color:#{colors[1]}}.syl-2{color:#{colors[2]}}"
-    else
-      ".syl-0,.syl-1,.syl-2{color:inherit}"
-    end
+                ".syl-0{color:#{colors[0]}}.syl-1{color:#{colors[1]}}.syl-2{color:#{colors[2]}}"
+              else
+                ".syl-0,.syl-1,.syl-2{color:inherit}"
+              end
 
     content = setting.syllable_mode ? TextFormatter.syllabify(text) : text
 
