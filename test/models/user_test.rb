6263397645f9cbed116c3d1527bc55d9af98f9a3
Tony Mouhete <tony.mouhete@gmail.com>
2026-08-23 13:06:12 +0200

    test: cover default user settings

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "creates default settings after registration" do
+    user = User.create!(
+      email: "user-test@example.com",
+      password: "password123"
+    )
+
+    assert_not_nil user.setting
+    assert_equal "OpenDyslexic", user.setting.font
+    assert_equal "12", user.setting.font_size
+    assert_not user.setting.syllable_mode
+  end
+
+  test "destroys associated articles" do
+    user = User.create!(
+      email: "destroy-test@example.com",
+      password: "password123"
+    )
+    article = user.articles.create!(title: "Test", content: "Contenu")
+
+    user.destroy!
+
+    assert_not Article.exists?(article.id)
+  end
 end
