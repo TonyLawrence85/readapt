1c9120e16865867429096c7f18c9e8cf0442b15c
Tony Mouhete <tony.mouhete@gmail.com>
2026-08-23 16:39:29 +0200

    test: cover article creation without external services

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -41,6 +41,27 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert Article.exists?(@other_article.id)
   end
 
+  test "creates an article for the signed in user" do
+    sign_in @user
+
+    with_fake_adaptation(["Texte adapté."]) do
+      assert_enqueued_with(job: AudioGenerationJob) do
+        assert_difference("@user.articles.count", 1) do
+          post articles_url, params: {
+            article: { title: "Nouveau texte", content: "Texte original", source: "copy" }
+          }
+        end
+      end
+    end
+
+    created_article = @user.articles.order(:created_at).last
+    assert_equal "Nouveau texte", created_article.title
+    assert_equal "Texte original", created_article.content
+    assert_equal "Texte adapté.", created_article.formatted_content
+    assert_response :redirect
+    assert_equal article_path(created_article), URI.parse(response.location).path
+  end
+
   test "toggles favourite on the users own article" do
     sign_in @user
 
@@ -71,5 +92,15 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :not_found
     assert_equal original_value, @other_article.reload.favourite?
+  end
+
+  private
+
+  def with_fake_adaptation(result)
+    original_call = ArticleAdaptationService.method(:call)
+    ArticleAdaptationService.define_singleton_method(:call) { |_content, **_options| result }
+    yield
+  ensure
+    ArticleAdaptationService.define_singleton_method(:call, original_call)
   end
 end
