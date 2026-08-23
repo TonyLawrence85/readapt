06147c593aae8a358526882a697b3ab6ac7fd140
Tony Mouhete <tony.mouhete@gmail.com>
2026-08-23 12:12:13 +0200

    refactor: simplify text to speech service

--- app/services/tts_service.rb
+++ app/services/tts_service.rb
@@ -1,26 +1,18 @@
 class TtsService
+  VOICE = { language_code: "fr-FR", ssml_gender: :NEUTRAL }.freeze
+  AUDIO_CONFIG = { audio_encoding: :MP3 }.freeze
+
   def self.call(text)
-    client = Google::Cloud::TextToSpeech.text_to_speech
-
-    # Configuration de l'entrée (le texte)
-    input_text = { text: text }
-
-    # Choix de la voix (Langue et genre)
-    voice = {
-      language_code: "fr-FR",
-      ssml_gender: :NEUTRAL
-    }
-
-    # Configuration du format de sortie
-    audio_config = { audio_encoding: :MP3 }
-
-    # Requête à l'API
     response = client.synthesize_speech(
-      input: input_text,
-      voice: voice,
-      audio_config: audio_config
+      input: { text: text },
+      voice: VOICE,
+      audio_config: AUDIO_CONFIG
     )
-
     response.audio_content
   end
+
+  def self.client
+    Google::Cloud::TextToSpeech.text_to_speech
+  end
+  private_class_method :client
 end
