0f8bed5dc2c7db44bd324f0c30d591366ed2876d
Tony Mouhete <tony.mouhete@gmail.com>
2026-08-23 13:35:11 +0200

    test: cover audio generation job guard clause

--- test/jobs/audio_generation_job_test.rb
+++ test/jobs/audio_generation_job_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class AudioGenerationJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.create!(email: "audio-job@example.com", password: "password123")
+  end
+
+  test "does nothing when formatted content is blank" do
+    article = @user.articles.create!(title: "Lecture", content: "Texte original")
+
+    AudioGenerationJob.perform_now(article.id)
+
+    assert_not article.reload.audio.attached?
+    assert_nil article.audio_timestamps
+  end
 end
